e3e032d76d132a2bfa4a0d0337f7374f553b7a4c
Vishal3257 <vt464670@gmail.com>
2026-05-05 09:35:54 +0530

    two step verification

--- Auth_Project/settings.py
+++ Auth_Project/settings.py
@@ -237,8 +237,8 @@ EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'vt464670@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'nczgwweqcanuoafs')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Django_OTP_API <vt464670@gmail.com>')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'ayqskqoznsunxizt')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Authapp <vt464670@gmail.com>')
 
 #DEFAULT_AUTO_FIELD = 'django_mongodb_backend.fields.ObjectIdAutoField'
 # MongoDB ke liye AutoField use kar rahe hain
